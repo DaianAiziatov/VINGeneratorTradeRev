e887598d63be5eef60ea7bbca9eb4e4caf3345c9
daianaiziatovTradeRev <daian.aiziatov@traderev.com>
2019-12-03 14:16:32 -0500

    Initial Commit

---
+++ VINGenerator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  VINGenerator
+//
+//  Created by Daian Aziatov on 2019-12-03.
+//  Copyright © 2019 Daian Aziatov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
