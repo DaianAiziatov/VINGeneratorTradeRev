cd2207b2bac0e2b100e39db78e2e7ec4b92e3066
DaianAiziatovTrade <daian.aiziatov@gmail.com>
2020-01-13 13:29:26 -0500

    Add appIcon

---
+++ VINGenerator-mac/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  VINGenerator-mac
+//
+//  Created by Daian Aziatov on 2020-01-13.
+//  Copyright © 2020 Daian Aziatov. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
